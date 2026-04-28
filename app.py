19afeb1dedb65b6367a5e6d4a1ca658e6a031ade
Jorge Alexander Sailema Torres <jsailema4049@uta.edu.ec>
2026-04-28 22:50:02 +0000

    error corregido

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from mssql_python import connect
 
 app = Flask(__name__)
 
 
-def get_connection():
+#def get_connection():
     server = os.getenv("DB_SERVER")
     database = os.getenv("DB_DATABASE")
     username = os.getenv("DB_USERNAME")
@@ -34,16 +34,16 @@ def get_connection():
     return connect(connection_string)
 
 
-@app.route("/")
-def home():
+#@app.route("/")
+#def home():
     return jsonify({
         "success": True,
         "message": "API Flask funcionando correctamente en Render"
     })
 
 
-@app.route("/test-db")
-def test_db():
+#@app.route("/test-db")
+#def test_db():
     conn = None
     cursor = None
     try:
@@ -69,8 +69,8 @@ def test_db():
 
 
 # 🔥 ENDPOINT CORREGIDO
-@app.route("/productos")
-def listar_productos():
+#@app.route("/productos")
+#def listar_productos():
     conn = None
     cursor = None
     try:
